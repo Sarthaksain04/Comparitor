59f68950d0590bd6ae097c8c65c1060d8169910c
Sarthak Sain <Sarthaksain08@gmail.com>
2026-02-23 09:57:18 +0530

    setup env

--- backend/login/config/db.js
+++ backend/login/config/db.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://sarthaksain:comparitor@finalyearproject.bo0vvuf.mongodb.net/?retryWrites=true&w=majority&appName=Finalyearproject",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      // "mongodb+srv://sarthaksain:comparitor@finalyearproject.bo0vvuf.mongodb.net/?retryWrites=true&w=majority&appName=Finalyearproject",
+      // {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+      // }
+      process.env.MONGO_URI
     );
     console.log("✅ Connected to MongoDB ");
   } catch (err) {
